2c5a8178eb6271b8e5d3723e9043498688399d3e
IvanARashid <ivan52x53@gmail.com>
2025-03-10 10:49:43 +0100

    Error heatmaps as subplots

--- figures/phd1_figs_reviewer_suggested.py
+++ figures/phd1_figs_reviewer_suggested.py
@@ -483,7 +483,7 @@ axs[1,1].set_xlabel("Nominal b-value [s/mm$^2$]")
 fig.text(0, 1, "(a)", fontsize=18)
 fig.text(0.515, 1, "(b)", fontsize=18)
 fig.tight_layout()
-# %% Error heatmaps for nominal xyz analysis
+# %% Error heatmaps for nominal xyz analysis 
 
 # Stepsizes
 resolutions = np.array([1e-3, 1.25e-3, 1.5e-3, 1.75e-3, 2e-3, 2.25e-3, 2.5e-3, 3e-3, 3.5e-3, 4e-3])
@@ -500,68 +500,92 @@ D_constant = 1e-3
 #bvalues_nominal_best, bvalues_actual_best = read_simulation_files_and_average(path_best, correction="crossterm_corrected", angles="xyz", xyres=resolutions, zres=resolutions)
 
 ### Varying f
-f_f_heatmap_estimates = np.zeros((f_range.shape[0], resolutions.shape[0]))
+f_f_heatmap_estimates_worst = np.zeros((f_range.shape[0], resolutions.shape[0]))
+f_f_heatmap_estimates_best = np.zeros((f_range.shape[0], resolutions.shape[0]))
 f_f_heatmap_truth = np.zeros((f_range.shape[0], resolutions.shape[0]))
 
-f_Dstar_heatmap_estimates = np.zeros((f_range.shape[0], resolutions.shape[0]))
+f_Dstar_heatmap_estimates_worst = np.zeros((f_range.shape[0], resolutions.shape[0]))
+f_Dstar_heatmap_estimates_best = np.zeros((f_range.shape[0], resolutions.shape[0]))
 f_Dstar_heatmap_truth = np.zeros((f_range.shape[0], resolutions.shape[0]))
 
-f_D_heatmap_estimates = np.zeros((f_range.shape[0], resolutions.shape[0]))
+f_D_heatmap_estimates_worst = np.zeros((f_range.shape[0], resolutions.shape[0]))
+f_D_heatmap_estimates_best = np.zeros((f_range.shape[0], resolutions.shape[0]))
 f_D_heatmap_truth = np.zeros((f_range.shape[0], resolutions.shape[0]))
 for i in range(f_range.shape[0]):
-    f_estimates, Dstar_estimates, D_estimates = powder_average_all_simulation_logs_new(path_worst, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_range[i], Dstar=Dstar_constant, D=D_constant)
+    f_estimates_worst, Dstar_estimates_worst, D_estimates_worst = powder_average_all_simulation_logs_new(path_worst, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_range[i], Dstar=Dstar_constant, D=D_constant)
+    f_estimates_best, Dstar_estimates_best, D_estimates_best = powder_average_all_simulation_logs_new(path_best, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_range[i], Dstar=Dstar_constant, D=D_constant)
 
-    f_f_heatmap_estimates[i, :] = f_estimates
+    f_f_heatmap_estimates_worst[i, :] = f_estimates_worst
+    f_f_heatmap_estimates_best[i, :] = f_estimates_best
     f_f_heatmap_truth[i, :] = f_range[i]
 
-    f_Dstar_heatmap_estimates[i, :] = Dstar_estimates
+    f_Dstar_heatmap_estimates_worst[i, :] = Dstar_estimates_worst
+    f_Dstar_heatmap_estimates_best[i, :] = Dstar_estimates_best
     f_Dstar_heatmap_truth[:, :] = Dstar_constant*1000
 
-    f_D_heatmap_estimates[i, :] = D_estimates
+    f_D_heatmap_estimates_worst[i, :] = D_estimates_worst
+    f_D_heatmap_estimates_best[i, :] = D_estimates_best
     f_D_heatmap_truth[:, :] = D_constant*1000
 
 ### Varying Dstar
-Dstar_f_heatmap_estimates = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
+Dstar_f_heatmap_estimates_worst = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
+Dstar_f_heatmap_estimates_best = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
 Dstar_f_heatmap_truth = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
 
-Dstar_Dstar_heatmap_estimates = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
+Dstar_Dstar_heatmap_estimates_worst = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
+Dstar_Dstar_heatmap_estimates_best = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
 Dstar_Dstar_heatmap_truth = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
 
-Dstar_D_heatmap_estimates = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
+Dstar_D_heatmap_estimates_worst = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
+Dstar_D_heatmap_estimates_best = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
 Dstar_D_heatmap_truth = np.zeros((Dstar_range.shape[0], resolutions.shape[0]))
 for i in range(Dstar_range.shape[0]):
-    f_estimates, Dstar_estimates, D_estimates = powder_average_all_simulation_logs_new(path_worst, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_constant, Dstar=Dstar_range[i], D=D_constant)
+    f_estimates_worst, Dstar_estimates_worst, D_estimates_worst = powder_average_all_simulation_logs_new(path_worst, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_constant, Dstar=Dstar_range[i], D=D_constant)
+    f_estimates_best, Dstar_estimates_best, D_estimates_best = powder_average_all_simulation_logs_new(path_best, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_constant, Dstar=Dstar_range[i], D=D_constant)
 
-    Dstar_f_heatmap_estimates[i, :] = f_estimates
+    Dstar_f_heatmap_estimates_worst[i, :] = f_estimates_worst
+    Dstar_f_heatmap_estimates_best[i, :] = f_estimates_best
     Dstar_f_heatmap_truth[:, :] = f_constant
 
-    Dstar_Dstar_heatmap_estimates[i, :] = Dstar_estimates
+    Dstar_Dstar_heatmap_estimates_worst[i, :] = Dstar_estimates_worst
+    Dstar_Dstar_heatmap_estimates_best[i, :] = Dstar_estimates_best
     Dstar_Dstar_heatmap_truth[i, :] = Dstar_range[i]*1000
 
-    Dstar_D_heatmap_estimates[i, :] = D_estimates
+    Dstar_D_heatmap_estimates_worst[i, :] = D_estimates_worst
+    Dstar_D_heatmap_estimates_best[i, :] = D_estimates_best
     Dstar_D_heatmap_truth[:, :] = D_constant*1000
 
 
 ### Varying D
-D_f_heatmap_estimates = np.zeros((D_range.shape[0], resolutions.shape[0]))
+D_f_heatmap_estimates_worst = np.zeros((D_range.shape[0], resolutions.shape[0]))
+D_f_heatmap_estimates_best = np.zeros((D_range.shape[0], resolutions.shape[0]))
 D_f_heatmap_truth = np.zeros((D_range.shape[0], resolutions.shape[0]))
 
-D_Dstar_heatmap_estimates = np.zeros((D_range.shape[0], resolutions.shape[0]))
+D_Dstar_heatmap_estimates_worst = np.zeros((D_range.shape[0], resolutions.shape[0]))
+D_Dstar_heatmap_estimates_best = np.zeros((D_range.shape[0], resolutions.shape[0]))
 D_Dstar_heatmap_truth = np.zeros((D_range.shape[0], resolutions.shape[0]))
 
-D_D_heatmap_estimates = np.zeros((D_range.shape[0], resolutions.shape[0]))
+D_D_heatmap_estimates_worst = np.zeros((D_range.shape[0], resolutions.shape[0]))
+D_D_heatmap_estimates_best = np.zeros((D_range.shape[0], resolutions.shape[0]))
 D_D_heatmap_truth = np.zeros((D_range.shape[0], resolutions.shape[0]))
 for i in range(D_range.shape[0]):
-    f_estimates, Dstar_estimates, D_estimates = powder_average_all_simulation_logs_new(path_worst, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_constant, Dstar=Dstar_constant, D=D_range[i])
+    f_estimates_worst, Dstar_estimates_worst, D_estimates_worst = powder_average_all_simulation_logs_new(path_worst, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_constant, Dstar=Dstar_constant, D=D_range[i])
+    f_estimates_best, Dstar_estimates_best, D_estimates_best = powder_average_all_simulation_logs_new(path_best, correction="uncorrected", angles="xyz", xyres=resolutions, zres=resolutions, f=f_constant, Dstar=Dstar_constant, D=D_range[i])
 
-    D_f_heatmap_estimates[i, :] = f_estimates
+    D_f_heatmap_estimates_worst[i, :] = f_estimates_worst
+    D_f_heatmap_estimates_best[i, :] = f_estimates_best
     D_f_heatmap_truth[:, :] = f_constant
 
-    D_Dstar_heatmap_estimates[i, :] = Dstar_estimates
+    D_Dstar_heatmap_estimates_worst[i, :] = Dstar_estimates_worst
+    D_Dstar_heatmap_estimates_best[i, :] = Dstar_estimates_best
     D_Dstar_heatmap_truth[:, :] = Dstar_constant*1000
 
-    D_D_heatmap_estimates[i, :] = D_estimates
+    D_D_heatmap_estimates_worst[i, :] = D_estimates_worst
+    D_D_heatmap_estimates_best[i, :] = D_estimates_best
     D_D_heatmap_truth[i, :] = D_range[i]*1000
+
+
+# %% Error heatmaps as imagegrid
 
 colormap = "BrBG"
 aspect = 0.2
@@ -593,13 +617,93 @@ grid[0].set_yticklabels([0, 10, 20, 30])
 Dstar = grid[1].imshow((Dstar_Dstar_heatmap_estimates-Dstar_Dstar_heatmap_truth)/Dstar_Dstar_heatmap_truth, origin="lower", vmin=0.1, vmax=1.0, aspect=aspect, interpolation="none")
 cax = grid.cbar_axes[1].colorbar(Dstar)
 cax.set_ticks([0.1, 1])
-grid[1].set_ylabel("Ground truth $f$ [\%]")
-grid[1].set_yticks([0, 15, 32, 49])
-grid[1].set_yticklabels([0, 10, 20, 30])
+grid[1].set_ylabel("Ground truth $D^*$ [µm$^2$/ms]")
+grid[1].set_yticks([0, 5, 27, 49])
+grid[1].set_yticklabels([5, 10, 30, 50])
 
 #D = grid[2].imshow(np.flip((D_D_heatmap_estimates-D_D_heatmap_truth)/D_D_heatmap_truth, axis=0), aspect=aspect)
 D = grid[2].imshow((D_D_heatmap_estimates-D_D_heatmap_truth)/D_D_heatmap_truth, origin="lower", aspect=aspect, interpolation="bilinear")
 grid.cbar_axes[2].colorbar(D)
 
+
+# %% Error heatmaps as subplots
+
+cbar_aspect = 10
+cbar_shrink = 0.72
+aspect = 0.2
+factor = 100 # To show relative errors as percentages
+
+fig, axs = plt.subplots(ncols=3, nrows=2, figsize=(9,6), sharex=True)
+
+norm = colors.TwoSlopeNorm(vmin=-0.2*factor, vcenter=0.*factor, vmax=1.0*factor)
+f = axs[0,0].imshow((f_f_heatmap_estimates_worst-f_f_heatmap_truth)/f_f_heatmap_truth*factor, origin="lower", cmap=colormap, norm=norm, aspect=aspect, interpolation="none")
+cbar = fig.colorbar(f, ax=axs[0,0], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+cbar.set_ticks([-0.2*factor, 0, 1*factor])
+cbar.set_label("Relative $f$ error [\%]")
+axs[0,0].set_ylabel("Ground truth $f$ [\%]")
+axs[0,0].set_yticks([0, 15, 32, 49])
+axs[0,0].set_yticklabels(np.array([0, 10, 20, 30]))
+
+
+Dstar = axs[0,1].imshow((Dstar_Dstar_heatmap_estimates_worst-Dstar_Dstar_heatmap_truth)/Dstar_Dstar_heatmap_truth*factor, origin="lower", vmin=0.1*factor, vmax=1.0*factor, aspect=aspect, interpolation="none")
+cbar = fig.colorbar(Dstar, ax=axs[0,1], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+cbar.set_ticks(np.array([0.1, 0.5, 1])*factor)
+cbar.set_label("Relative $D^*$ error [\%]")
+axs[0,1].set_ylabel("Ground truth $D^*$ [µm$^2$/ms]")
+axs[0,1].set_yticks([0, 5, 27, 49])
+axs[0,1].set_yticklabels([5, 10, 30, 50])
+
+D = axs[0,2].imshow((D_D_heatmap_estimates_worst-D_D_heatmap_truth)/D_D_heatmap_truth*factor, origin="lower", vmin=0.02*factor, vmax=0.1*factor, aspect=aspect, interpolation="none")
+cbar = fig.colorbar(D, ax=axs[0,2], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+cbar.set_ticks(np.array([0.02, 0.05, 0.1])*factor)
+cbar.set_label("Relative $D$ error [\%]")
+axs[0,2].set_ylabel("Ground truth $D$ [µm$^2$/ms]")
+axs[0,2].set_yticks([0, 10, 30, 49])
+axs[0,2].set_yticklabels([0.5, 1, 2, 3])
+
+### Optimal sequence
+norm = colors.TwoSlopeNorm(vmin=-0.2*factor, vcenter=0.*factor, vmax=1.0*factor)
+f = axs[1,0].imshow((f_f_heatmap_estimates_best-f_f_heatmap_truth)/f_f_heatmap_truth*factor, origin="lower", cmap=colormap, norm=norm, aspect=aspect, interpolation="none")
+cbar = fig.colorbar(f, ax=axs[1,0], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+cbar.set_ticks([-0.2*factor, 0, 1*factor])
+#cbar.set_label("Relative $f$ error [\%]")
+axs[1,0].set_ylabel("Ground truth $f$ [\%]")
+axs[1,0].set_yticks([0, 15, 32, 49])
+axs[1,0].set_yticklabels(np.array([0, 10, 20, 30]))
+
+
+Dstar = axs[1,1].imshow((Dstar_Dstar_heatmap_estimates_best-Dstar_Dstar_heatmap_truth)/Dstar_Dstar_heatmap_truth*factor, origin="lower", cmap="viridis_r", vmin=-0.7*factor, vmax=0*factor, aspect=aspect, interpolation="none")
+cbar = fig.colorbar(Dstar, ax=axs[1,1], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+cbar.set_ticks(np.array([-0.7, -0.35, 0])*factor)
+#cbar.set_label("Relative $D^*$ error [\%]")
+axs[1,1].set_ylabel("Ground truth $D^*$ [µm$^2$/ms]")
+axs[1,1].set_yticks([0, 5, 27, 49])
+axs[1,1].set_yticklabels([5, 10, 30, 50])
+
+norm = colors.TwoSlopeNorm(vmin=-0.1*factor, vcenter=0.*factor, vmax=0.05*factor)
+D = axs[1,2].imshow((D_D_heatmap_estimates_best-D_D_heatmap_truth)/D_D_heatmap_truth*factor, origin="lower", norm=norm, cmap=colormap, aspect=aspect, interpolation="none")
+cbar = fig.colorbar(D, ax=axs[1,2], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+cbar.set_ticks(np.array([-0.1, 0., 0.05])*factor)
+#cbar.set_label("Relative $D$ error [\%]")
+axs[1,2].set_ylabel("Ground truth $D$ [µm$^2$/ms]")
+axs[1,2].set_yticks([0, 10, 30, 49])
+axs[1,2].set_yticklabels([0.5, 1, 2, 3])
+
+axs[1,0].set_xlabel("Isotropic resolution [mm]")
+axs[1,0].set_xticks([0, 4, 7, 9])
+axs[1,0].set_xticklabels([1, 2, 3, 4])
+
+axs[1,1].set_xlabel("Isotropic resolution [mm]")
+axs[1,1].set_xticks([0, 4, 7, 9])
+axs[1,1].set_xticklabels([1, 2, 3, 4])
+
+axs[1,2].set_xlabel("Isotropic resolution [mm]")
+axs[1,2].set_xticks([0, 4, 7, 9])
+axs[1,2].set_xticklabels([1, 2, 3, 4])
+
+
+fig.text(0.01, 0.95, "(a)", fontsize=18)
+fig.text(0.01, 0.47, "(b)", fontsize=18)
+fig.tight_layout()
 
 # %%
