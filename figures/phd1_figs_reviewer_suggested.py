2f1f3f0b9bc01c986bc7771ed28e271bce9ffac2
IvanARashid <ivan52x53@gmail.com>
2025-03-27 15:40:51 +0100

    Adjusted labels,, resolutions

--- figures/phd1_figs_reviewer_suggested.py
+++ figures/phd1_figs_reviewer_suggested.py
@@ -286,6 +286,7 @@ class CurvedText(mtext.Text):
 # %% b0 vs isotropic resolution
 
 resolutions = np.array([1e-3, 1.25e-3, 1.5e-3, 1.75e-3, 2e-3, 2.25e-3, 2.5e-3, 3e-3, 3.5e-3, 4e-3])
+resolutions = np.round(np.linspace(1,4,13)*1e-3, decimals=7)
 
 bvalues_nominal_worst, bvalues_actual_worst = read_simulation_files_and_average(path_worst, correction="crossterm_corrected", angles="xyz", xyres=resolutions, zres=resolutions)
 bvalues_nominal_best, bvalues_actual_best = read_simulation_files_and_average(path_best, correction="crossterm_corrected", angles="xyz", xyres=resolutions, zres=resolutions)
@@ -650,7 +651,7 @@ f = axs[0,0].imshow((f_f_heatmap_estimates_worst-f_f_heatmap_truth)/f_f_heatmap_
 cbar = fig.colorbar(f, ax=axs[0,0], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
 cbar.set_ticks([-0.2*factor, 0, 1*factor])
 cbar.set_label("Relative $f$ error [\%]")
-axs[0,0].set_ylabel("True $f$ [\%]")
+axs[0,0].set_ylabel("$f$ [\%]")
 axs[0,0].set_yticks([0, 15, 32, 49])
 axs[0,0].set_yticklabels(np.array([0, 10, 20, 30]))
 
@@ -660,7 +661,7 @@ Dstar = axs[0,1].imshow((Dstar_Dstar_heatmap_estimates_worst-Dstar_Dstar_heatmap
 cbar = fig.colorbar(Dstar, ax=axs[0,1], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
 cbar.set_ticks(np.array([-1, -0.5, 0, 0.5, 1])*factor)
 cbar.set_label("Relative $D^*$ error [\%]")
-axs[0,1].set_ylabel("True $D^*$ [µm$^2$/ms]")
+axs[0,1].set_ylabel("$D^*$ [µm$^2$/ms]")
 axs[0,1].set_yticks([0, 5, 27, 49])
 axs[0,1].set_yticklabels([5, 10, 30, 50])
 
@@ -669,35 +670,35 @@ D = axs[0,2].imshow((D_D_heatmap_estimates_worst-D_D_heatmap_truth)/D_D_heatmap_
 cbar = fig.colorbar(D, ax=axs[0,2], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
 cbar.set_ticks(np.array([-0.1, -0.05, 0, 0.05, 0.1])*factor)
 cbar.set_label("Relative $D$ error [\%]")
-axs[0,2].set_ylabel("True $D$ [µm$^2$/ms]")
+axs[0,2].set_ylabel("$D$ [µm$^2$/ms]")
 axs[0,2].set_yticks([0, 10, 30, 49])
 axs[0,2].set_yticklabels([0.5, 1, 2, 3])
 
 ### Optimal sequence
 norm = colors.TwoSlopeNorm(vmin=-0.2*factor, vcenter=0.*factor, vmax=1.0*factor)
 f = axs[1,0].imshow((f_f_heatmap_estimates_best-f_f_heatmap_truth)/f_f_heatmap_truth*factor, origin="lower", cmap=colormap, norm=norm, aspect=aspect, interpolation="none")
-cbar = fig.colorbar(f, ax=axs[1,0], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
-cbar.set_ticks([-0.2*factor, 0, 1*factor])
+#cbar = fig.colorbar(f, ax=axs[1,0], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+#cbar.set_ticks([-0.2*factor, 0, 1*factor])
 #cbar.set_label("Relative $f$ error [\%]")
-axs[1,0].set_ylabel("Ground truth $f$ [\%]")
+axs[1,0].set_ylabel("$f$ [\%]")
 axs[1,0].set_yticks([0, 15, 32, 49])
 axs[1,0].set_yticklabels(np.array([0, 10, 20, 30]))
 
 norm = colors.TwoSlopeNorm(vmin=-1.0*factor, vcenter=0.*factor, vmax=1.0*factor)
 Dstar = axs[1,1].imshow((Dstar_Dstar_heatmap_estimates_best-Dstar_Dstar_heatmap_truth)/Dstar_Dstar_heatmap_truth*factor, origin="lower", norm=norm, cmap=colormap, aspect=aspect, interpolation="none")
-cbar = fig.colorbar(Dstar, ax=axs[1,1], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
-cbar.set_ticks(np.array([-1, -0.5, 0, 0.5, 1])*factor)
+#cbar = fig.colorbar(Dstar, ax=axs[1,1], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+#cbar.set_ticks(np.array([-1, -0.5, 0, 0.5, 1])*factor)
 #cbar.set_label("Relative $D^*$ error [\%]")
-axs[1,1].set_ylabel("Ground truth $D^*$ [µm$^2$/ms]")
+axs[1,1].set_ylabel("$D^*$ [µm$^2$/ms]")
 axs[1,1].set_yticks([0, 5, 27, 49])
 axs[1,1].set_yticklabels([5, 10, 30, 50])
 
 norm = colors.TwoSlopeNorm(vmin=-0.1*factor, vcenter=0.*factor, vmax=0.1*factor)
 D = axs[1,2].imshow((D_D_heatmap_estimates_best-D_D_heatmap_truth)/D_D_heatmap_truth*factor, origin="lower", norm=norm, cmap=colormap, aspect=aspect, interpolation="none")
-cbar = fig.colorbar(D, ax=axs[1,2], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
-cbar.set_ticks(np.array([-0.1, -0.05, 0., 0.05, 0.1])*factor)
+#cbar = fig.colorbar(D, ax=axs[1,2], location="top", shrink=cbar_shrink, aspect=cbar_aspect)
+#cbar.set_ticks(np.array([-0.1, -0.05, 0., 0.05, 0.1])*factor)
 #cbar.set_label("Relative $D$ error [\%]")
-axs[1,2].set_ylabel("Ground truth $D$ [µm$^2$/ms]")
+axs[1,2].set_ylabel("$D$ [µm$^2$/ms]")
 axs[1,2].set_yticks([0, 10, 30, 49])
 axs[1,2].set_yticklabels([0.5, 1, 2, 3])
 
